9bad042bbb7e71c2fce6686f022c76a2b2e9d950
tbs03171 <tbs03171@naver.com>
2024-06-21 10:50:16 +0900

    Initial commit

---
+++ src/main/java/com/eeumteo/eeumteo_backend/EeumteoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eeumteo.eeumteo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeumteoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeumteoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeumteo/eeumteo_backend/EeumteoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeumteo.eeumteo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeumteoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
