fbc945f7fad22ae471f73e19a33b63c030e551cc
tbs03171 <tbs03171@naver.com>
2024-07-10 16:30:39 +0900

    feat: String 을 Gender 로 변환하는 메서드 추가

--- src/main/java/com/eeumteo/eeumteo_backend/account/entity/Gender.java
+++ src/main/java/com/eeumteo/eeumteo_backend/account/entity/Gender.java
@@ -2,4 +2,11 @@ package com.eeumteo.eeumteo_backend.account.entity;
 
 public enum Gender {
     MALE, FEMALE;
+
+    /**
+     * String 을 Gender 로 변환해서 반환
+     */
+    public static Gender from(String s) {
+        return Gender.valueOf(s.toUpperCase());
+    }
 }
