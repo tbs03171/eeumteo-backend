97d3cf408c81ce2a4f3a8d92bd3593aefabc347d
tbs03171 <tbs03171@naver.com>
2024-07-10 16:26:20 +0900

    #2 feat: AuthController 로그인, 회원가입 api 구현

---
+++ src/main/java/com/eeumteo/eeumteo_backend/account/dto/SignInAccountForm.java
@@ -0,0 +1,16 @@
+package com.eeumteo.eeumteo_backend.account.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInAccountForm {
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+}
---
+++ src/main/java/com/eeumteo/eeumteo_backend/account/dto/SignUpAccountResponse.java
@@ -0,0 +1,12 @@
+package com.eeumteo.eeumteo_backend.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpAccountResponse {
+    private Long accountId;
+}
